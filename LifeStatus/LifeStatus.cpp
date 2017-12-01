ef53313aeb4f8dd6671e66d276e4fc521c495066
Darvance <jhmgraaf@gmail.com>
2017-12-01 14:50:53 +0100

    updated Initial Setup Module V1

--- LifeStatus/LifeStatus.cpp
+++ LifeStatus/LifeStatus.cpp
@@ -1,4 +1,23 @@
 #include "LifeStatus.h"
-
-
 #include "Arduino.h"
+#include <EEPROM.h>
+
+
+LifeStatus::LifeStatus(uint64_t signature)
+{
+    LS_location_sig = 0;
+    EEPROM.put(LS_location_sig, signature);
+}
+
+uint64_t LifeStatus::init()
+{   
+    uint64_t LS_signature;
+    EEPROM.get(LS_location_sig,LS_signature);
+    return LS_signature;
+}
+
+void LifeStatus::initHash(uint64_t hash)
+{
+    LS_location_hash = sizeof(uint64_t);
+    EEPROM.put(LS_location_hash,hash);
+}
--- LifeStatus/LifeStatus.h
+++ LifeStatus/LifeStatus.h
@@ -1,25 +1,28 @@
 #ifndef LifeStatus_h
 #define LifeStatus_h
 
+#if(ARDUINO>= 100)
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
 #include <inttypes.h>
 
 class LifeStatus
 {
 public:
-	LifeStatus(uint64_t signiture);
+	LifeStatus(uint64_t);
 	uint64_t init();
 	void initHash(uint64_t);
 
 	void listenToBroadcast();
 	void updateHashes();
 
-		
-
-
 private:
-	uint64_t LS_signiture;
-	uint64_t LS_Alive_Hash;
-
+	int LS_location_sig;
+	int LS_location_hash;
 	void respondToBroadcast();
 
 };
